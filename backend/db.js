9f016f1429494ee25b3e388fa97995dc8f268fbd
Myguel02 <myguel.amorim@gmail.com>
2026-04-28 10:46:33 -0300

    acesso ao banco

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   port: Number(process.env.DB_PORT || 3306),
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
+  password: process.env.DB_PASSWORD || "M@cieira22",
   database: process.env.DB_NAME || "servicos",
 });
 
